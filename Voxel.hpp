aee0b9906b3a5990bf2827e88da867196fed63b4
Nanzil <86268071+Nanzil@users.noreply.github.com>
2021-07-23 08:42:42 -0300

    Update Voxel.hpp

--- Voxel.hpp
+++ Voxel.hpp
@@ -1,30 +1,17 @@
-#ifndef SCULPTOR_HPP
-#define SCULPTOR_HPP
-#include "voxel.hpp"
+#ifndef VOXEL_HPP
+#define VOXEL_HPP
 
-/// @brief Class to manipulate the voxels
-/// @param v Voxel 3D matrix
-/// @param nx, ny, nz Matrix dimensions
-/// @param r, g, b, a Current drawing color
-class Sculptor
+////@short Volume element
+/// @param r red color intensity.
+/// @param g green color intensity.
+/// @param b blue color intensity.
+/// @param a element tranparency.
+/// @param isOn show if the Voxel is active.
+struct Voxel
 {
-protected:
-  Voxel ***v;
-  int nx, ny, nz;
-  float r, g, b, a;
-public:
-  Sculptor(int _nx, int _ny, int _nz);
-  ~Sculptor();
-  void setColor(float r, float g, float b, float alpha);
-  void putVoxel(int x, int y, int z);
-  void cutVoxel(int x, int y, int z);
-  void putBox(int x0, int x1, int y0, int y1, int z0, int z1);
-  void cutBox(int x0, int x1, int y0, int y1, int z0, int z1);
-  void putSphere(int xcenter, int ycenter, int zcenter, int radius);
-  void cutSphere(int xcenter, int ycenter, int zcenter, int radius);
-  void putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz);
-  void cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz);
-  void writeOFF(char *filename);
+  float r, g, b;
+  float a;
+  bool isOn;
 };
 
-#endif // SCULPTOR_HPP
+#endif // VOXEL_HPP
